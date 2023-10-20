9c101f3ecbf0d2e95109b86f2ec2f4ab0f825732
9793 <estebitangb12@gmail.com>
2023-10-20 18:04:15 +0200

    cuarto commit

--- app/src/main/java/com/example/albondigas/data/local/TapasLocalDataSource.kt
+++ app/src/main/java/com/example/albondigas/data/local/TapasLocalDataSource.kt
@@ -19,7 +19,7 @@ class TapasLocalDataSource (
     fun getTapas():Either<ErrorApp,Tapas>{
         val jsonTapas=sharedPref.getString(tapasId,null)
         jsonTapas?.let {
-            return  serialization.fromJson(it,Tapas::class.java).right()
+            return serialization.fromJson(it,Tapas::class.java).right()
         }
         return  ErrorApp.DataError.left()
     }
